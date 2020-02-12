e68e041d39c5709a0bd08b23993decd796458f0f
Theodore <theo.stefou@gmail.com>
2020-02-12 11:16:00 +0200

    Added predictions for 2b

--- 2a/2a-duplicate_detection_LSH_random_projection.py
+++ 2a/2a-duplicate_detection_LSH_random_projection.py
@@ -7,7 +7,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-hash_size = 3
+hash_size = 4
 
 
 class HashTable:
