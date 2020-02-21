4719b886658029871773e80905df1a0534dcd6d1
giorgospan <giorgospan96@gmail.com>
2020-02-22 01:15:55 +0200

    Slight modification on wordcloud.py

--- 1a/1a-wordcloud.py
+++ 1a/1a-wordcloud.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 
 from wordcloud import WordCloud
 
@@ -10,6 +11,7 @@ from nltk.corpus import stopwords
 punkt = {',', '.', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '/', '\\', '-', '_', '+', '=', '"', '~', '`', '<', '>', '?', ':', ';', "'", '|', '[', ']', '{', '}', '*'}
 stop_words = set(stopwords.words('english'))
 
+stop_words = set(stopwords.words('english'))
 with open('../extras/extra_stopwords.txt') as f:
     for line in f:
         stop_words.add(line[:-1])
@@ -20,45 +22,53 @@ wordcloud_dict = {'Business': [], 'Entertainment': [], 'Health': [], 'Technology
 #------------Read the csv file and change the encoding----------------
 df  = pd.read_csv('../datasets/q1/train.csv', encoding='utf-8')
 #Uncomment to test without waiting too long...
-#df = df[:1000]
+# df = df[:100]
 df['Title'] = df['Title'].str.encode('ascii', 'ignore').str.decode('ascii')
 df['Content'] = df['Content'].str.encode('ascii', 'ignore').str.decode('ascii')
 df['Label'] = df['Label'].str.encode('ascii', 'ignore').str.decode('ascii')
 #---------------------------------------------------------------------
 
 #--------------- HELPER FUNCTIONS -------------------
-def remove_punkt(s):
-    global punkt
-    for letter in s:
-        if letter in punkt:
-            return False
-    return True
+# def remove_punkt(s):
+#     global punkt
+#     for letter in s:
+#         if letter in punkt:
+#             return False
+#     return True
 
-def is_not_stop_word(word):
-    global stop_words
-    return word not in stop_words
+# def is_not_stop_word(word):
+#     global stop_words
+#     return word not in stop_words
 #----------------------------------------------------
 
-for index, row in df.iterrows():
-    title = row['Title']
-    content = row['Content']
-    label = row['Label']
+# for index, row in df.iterrows():
+#     title = row['Title']
+#     content = row['Content']
+#     label = row['Label']
 
-    toAdd = ''
+#     toAdd = ''
 
-    words = word_tokenize(title)
-    words = filter(remove_punkt, words)
-    words = filter(is_not_stop_word, words)
-    toAdd += (' '.join(words))*20
+#     words = word_tokenize(title)
+#     words = filter(remove_punkt, words)
+#     words = filter(is_not_stop_word, words)
+#     toAdd += (' '.join(words))*20
 
-    toAdd += ' '
+#     toAdd += ' '
 
-    words = word_tokenize(content)
-    words = filter(remove_punkt, words)
-    words = filter(is_not_stop_word, words)
-    toAdd += ' '.join(words)
+#     words = word_tokenize(content)
+#     words = filter(remove_punkt, words)
+#     words = filter(is_not_stop_word, words)
+#     toAdd += ' '.join(words)
 
-    wordcloud_dict[label].append(toAdd)
+#     wordcloud_dict[label].append(toAdd)
+
+df['Content'] = 20*df['Title']+df['Content']
+
+df['Content'] = df['Content'].apply(lambda x:re.sub(r'[^\w\s]',' ',x))
+df['Content'] = df['Content'].apply(word_tokenize)
+df['Content'] = df['Content'].apply(lambda x:[word for word in x if word not in (stop_words) and len(word)>1])
+df['Content'] = df['Content'].apply(lambda x:' '.join(x))
+wordcloud_dict = df.groupby('Label')['Content'].apply(list).to_dict()
 
 for category in wordcloud_dict:
     wordcloud = WordCloud(width=1920, height=1080).generate_from_text(' '.join(wordcloud_dict[category]))
